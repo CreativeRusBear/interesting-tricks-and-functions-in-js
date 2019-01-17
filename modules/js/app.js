38240e3f156e608e95125bf5cf1fcfcd2d3d63d8
Artem <gusev2014russia@mail.ru>
2019-01-17 16:45:17 +0300

    Добавлена работа с модулями

---
+++ modules/js/app.js
@@ -0,0 +1,4 @@
+import * as math from './math.js';
+console.log(`Surface of Mars: ${math.surfaceArea(3390)}`);
+console.log(`Surface of Mercury: ${math.surfaceArea(2240)}`);
+console.log(`Year Squared: ${math.square(2019)}`);
---
+++ modules/js/math.js
@@ -0,0 +1,5 @@
+const pi = 3.14;
+const e = 2.718;
+const square = a => a * a;
+const surfaceArea = (r) => 4 * pi * square(r);
+export {pi,e,square,surfaceArea};
